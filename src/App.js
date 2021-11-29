189c7c7ead273c66ddee67fbcb1b53ed229f1062
ACwolf55 <66705643+ACwolf55@users.noreply.github.com>
2021-11-29 16:25:44 -0700

    cleaned up app

--- src/App.js
+++ src/App.js
@@ -6,10 +6,10 @@ import MainDisplay from './Components/MainDisplay';
 function App() {
   return (
     <div className="App">
+
       <Header/>
-      <div className='display'>
       <MainDisplay/>
-      </div>
+      
 
     </div>
   );
